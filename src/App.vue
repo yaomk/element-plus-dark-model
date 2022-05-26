fc044e311be1a6a6fa3d6d3a20898fe8163b57d7
yaomk <yao34209@163.com>
2022-05-26 17:55:41 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,37 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <br>
+    <el-button>tst</el-button>
+    <el-table :data="list" stripe>
+      <el-table-column label="tset" prop="test"></el-table-column>
+      <el-table-column label="tset" prop="test"></el-table-column>
+      <el-table-column label="tset" prop="test"></el-table-column>
+      <el-table-column label="tset" prop="test"></el-table-column>
+    </el-table>
   </nav>
   <router-view/>
 </template>
-
+<script setup>
+import { ref } from 'vue'
+const list = ref([
+  {
+    test: '1'
+  },
+  {
+    test: '1'
+  },
+  {
+    test: '1'
+  },
+  {
+    test: '1'
+  },
+  {
+    test: '1'
+  }
+])
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).mount('#app')
+import 'element-plus/theme-chalk/dark/css-vars.css'
+// import dark from 'element-plus/theme-chalk/dark/css-vars.css'
+
+import App from './App.vue'
+// console.log(dark)
+
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(router).mount('#app')
